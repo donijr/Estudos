0020a55e9d517e4305068aa7ad422affcbd3f932
donijr <donizetigmjr@gmail.com>
2020-02-23 11:27:30 -0300

    Comentário adicionado

--- ComputacaoGrafica/Turtle/poligono_regular.py
+++ ComputacaoGrafica/Turtle/poligono_regular.py
@@ -1,6 +1,7 @@
 # função que desenha um poligono regular de n lados
-# poligono_regular(numero_lados,comprimeto_lado) . numero_lados e comprimento_lados devem ser inteiros
-# Dependendo do número de lados a função arredonda o número do ângulo para um inteiro.
+# poligono_regular(numero_lados,comprimeto_lado) . numero_lados  deve ser inteiro e 
+# comprimento_lados pode ser inteiro ou float
+
 
 import turtle
 
@@ -10,16 +11,12 @@ caneta = turtle.Turtle()
 def poligono_regular(n_lados, comp_lado):
     angulo_total = 360/n_lados
     angulo_parcial = 360/n_lados
-    """ for i in range(n_lados,720,n_lados):
-        caneta.forward(comp_lado)
-        caneta.setheading(i)  """
-
     while(angulo_total <= 361):
         caneta.forward(comp_lado)
         caneta.setheading(angulo_total)
         angulo_total = angulo_total + angulo_parcial
 
-poligono_regular(7,60)
+poligono_regular(7.5,60)
 
  
 input()

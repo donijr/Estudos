3696509cf75e0af19db5f4b441b3ec332acc882b
donijr <donizetigmjr@gmail.com>
2020-02-29 18:29:06 -0300

    Otimização do código

--- ComputacaoGrafica/Turtle/poligono_regular_3d.py
+++ ComputacaoGrafica/Turtle/poligono_regular_3d.py
@@ -1,48 +1,42 @@
+#n_lados = número de lados do polígono. n_lados deve ser do tipo inteiro
+#comp_lado = comprimento do lado do polígono. comp_lado pode ser tipo inteiro ou float
+#profundidade = profundidade do polígonp. profundidade pode ser tipo inteiro ou float
+#turtle = objeto do tipo turtle que vai desenhar o polígono
+
 import turtle
 
 caneta = turtle.Turtle()
 
-
-def poligono_regular(n_lados, comp_lado,turtle):
+def poligono_regular_3d(n_lados, comp_lado,profundidade,turtle):
+    posicao_desenho_inicial = turtle.pos()
     acumulador_angulo = 360/n_lados
     angulo_minimo = 360/n_lados
-    while(acumulador_angulo <= 361):
-        turtle.forward(comp_lado)
-        turtle.setheading(acumulador_angulo)
-        acumulador_angulo = acumulador_angulo + angulo_minimo
 
-def poligono_regular_3d(n_lados, comp_lado,deslocamento,turtle):
-    acumulador_angulo = 360/n_lados
-    angulo_minimo = 360/n_lados
     while(acumulador_angulo <= 361):
-        x_atual = turtle.xcor()
-        y_atual = turtle.ycor()
-        x_temp = x_atual + deslocamento
-        y_temp = y_atual + deslocamento
-        turtle.setpos(x_temp,y_temp)
+        posicao_poligono = turtle.pos()
+        posicao_linha = posicao_poligono + (profundidade,profundidade)
+        turtle.setpos(posicao_linha)
         turtle.penup()
-        turtle.setpos(x_atual,y_atual)
+        turtle.setpos(posicao_poligono)
         turtle.pendown()
         turtle.forward(comp_lado)
         turtle.setheading(acumulador_angulo)
-        acumulador_angulo = acumulador_angulo + angulo_minimo
-caneta.setpos(100,100)
-print(caneta.pos())
-posicao_inicial = caneta.pos()
-nova_pos = posicao_inicial + (50.00,50.00)
-print(nova_pos)
-
-""" deslocamento = 70
-caneta.speed(1)
-poligono_regular_3d(6,100,70,caneta)
-caneta.speed(1)
-caneta.setpos(deslocamento,deslocamento)
-caneta.speed(1)
-poligono_regular(6,100,caneta)
-caneta.end_fill()
- """
+        #acumulador_angulo = acumulador_angulo + angulo_minimo
+        acumulador_angulo += angulo_minimo
 
 
+    nova_posicao_desenho = posicao_desenho_inicial + (profundidade,profundidade)
+    turtle.setpos(nova_posicao_desenho)
 
+    acumulador_angulo = 360/n_lados
+    while(acumulador_angulo <= 361):
+        turtle.forward(comp_lado)
+        turtle.setheading(acumulador_angulo)
+        #acumulador_angulo = acumulador_angulo + angulo_minimo
+        acumulador_angulo += angulo_minimo
+
+caneta.setpos(-50,-50)
+
+poligono_regular_3d(5,100.587,50.4587,caneta)
 
 input()

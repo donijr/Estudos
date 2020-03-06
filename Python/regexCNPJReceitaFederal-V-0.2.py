a07da5080982e4f7e80b54782fd6fbc87b5de224
donijr <donizetigmjr@gmail.com>
2020-03-06 17:49:28 -0300

    "Funcão melhorada"

--- Python/regexCNPJReceitaFederal-V-0.2.py
+++ Python/regexCNPJReceitaFederal-V-0.2.py
@@ -1,12 +1,14 @@
 import re
 
 def substituir(campo):
+    str(campo)
+    tam = len(campo)
     arquivo = open('exemplo','r')
     text_unico = arquivo.read()
     campo = re.search('"{}*[ -~]*?,'.format(campo),text_unico)
 
     nome_campo = campo.group(0)
-    print(nome_campo)
+    #print(nome_campo)
 
     novo_campo=[]
 
@@ -14,15 +16,14 @@ def substituir(campo):
 
     for letra in nome_campo:
         if (letra != "\"") and  (letra != ":") and (letra != ","):
-            print(letra)
             novo_campo.append(letra)
-
+    
+    # limpando palavra do tipo do campo
     novo_campo = ''.join(novo_campo)
+    str(novo_campo)
+    novo_campo = novo_campo[tam:]
     arquivo.close
 
     return novo_campo
 
-substituir("municipio")
-
-
-
+print(substituir("telefone"))

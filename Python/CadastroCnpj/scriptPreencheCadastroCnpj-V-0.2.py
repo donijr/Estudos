96cc9319fe60d483b1b0a5861a9246c414201215
donijr <donizetigmjr@gmail.com>
2020-03-08 18:14:02 -0300

    função melhorada

--- Python/CadastroCnpj/scriptPreencheCadastroCnpj-V-0.2.py
+++ Python/CadastroCnpj/scriptPreencheCadastroCnpj-V-0.2.py
@@ -3,26 +3,26 @@ import re
 def substituir(campo):
     str(campo)
     tam = len(campo)
-    with open('exemplo','r') as arquivo:
+    arquivo = open('exemplo','r')
+    text_unico = arquivo.read()
+    campo = re.search('"{}*[ -~]*?,'.format(campo),text_unico)
+
+    nome_campo = campo.group(0)
+    #print(nome_campo)
+
+    novo_campo=[]
+
+    # Limpando String
+
+    for letra in nome_campo:
+        if (letra != "\"") and  (letra != ":") and (letra != ","):
+            novo_campo.append(letra)
     
-        text_unico = arquivo.read()
-        campo = re.search('"{}*[ -~]*?,'.format(campo),text_unico)
-
-        nome_campo = campo.group(0)
-        #print(nome_campo)
-
-        novo_campo=[]
-
-        # Limpando String
-
-        for letra in nome_campo:
-            if (letra != "\"") and  (letra != ":") and (letra != ","):
-                novo_campo.append(letra)
-        
-        # limpando palavra do tipo do campo
-        novo_campo = ''.join(novo_campo)
-        str(novo_campo)
-        novo_campo = novo_campo[tam:]
+    # limpando palavra do tipo do campo
+    novo_campo = ''.join(novo_campo)
+    str(novo_campo)
+    novo_campo = novo_campo[tam:]
+    arquivo.close
 
     return novo_campo
 
